83dea2c0c4d7048de0c1eeb5a048cd20d0035961
Behnood Marvazi <dr.marvazi@gmail.com>
2013-07-14 23:42:32 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,12 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var fs = require('fs');
-	var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
-	console.log(buf.toString());
+    response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+  var express = require('express');
+
 });
